5d86e33ff20c5844ddaccf274e3ec1a1502ee5da
Mac Siri <krairit.siri@gmail.com>
2020-09-10 20:22:17 -0400

    Use a less random filename in ArticleImageUploader

--- app/uploaders/article_image_uploader.rb
+++ app/uploaders/article_image_uploader.rb
@@ -4,6 +4,13 @@ class ArticleImageUploader < BaseUploader
   end
 
   def filename
-    "#{Array.new(20) { rand(36).to_s(36) }.join}.#{file.extension}" if original_filename.present?
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
   end
 end
