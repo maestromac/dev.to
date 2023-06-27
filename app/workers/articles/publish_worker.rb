a542c7b7afdc15af5b70d071046e7d4119ee325a
Mac Siri <krairit.siri@gmail.com>
2023-06-27 13:49:53 -0400

    Update PublishWorker

--- app/workers/articles/publish_worker.rb
+++ app/workers/articles/publish_worker.rb
@@ -7,7 +7,7 @@ module Articles
     def perform
       # find published articles for which notifications were not set yet (so the context notifications were not created)
       published_articles = Article.where(published: true, published_at: 30.minutes.ago..Time.current)
-        .where.missing(:context_notifications_published)
+        .where.missing(:context_notifications)
       published_articles.each do |article|
         # send slack notifications
         Slack::Messengers::ArticlePublished.call(article: article)
