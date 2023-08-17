8c55ebbdbcdf6efaabee3e9708583f5efe47833b
Mac Siri <krairit.siri@gmail.com>
2023-08-17 12:43:35 -0400

    Try using rtx directly

--- gitpod/gitpod-init.sh
+++ gitpod/gitpod-init.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-npm install -g yarn
+rtx x node -- npm install -g yarn
 cp .env_sample .env
 echo "APP_DOMAIN=\"$(gp url 3000 | cut -f 3 -d /)\"" >> .env
 echo 'APP_PROTOCOL="https://"' >> .env
-gem install foreman
+rtx x ruby -- gem install foreman
 bin/setup
 bin/webpack
