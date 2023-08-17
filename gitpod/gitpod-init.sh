2036a8d6c11d1937d668132ba4e756cda4f71287
Mac Siri <krairit.siri@gmail.com>
2023-08-17 14:52:17 -0400

    Revert rtx usages

--- gitpod/gitpod-init.sh
+++ gitpod/gitpod-init.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-rtx x node -- npm install -g yarn
+npm install -g yarn
 cp .env_sample .env
 echo "APP_DOMAIN=\"$(gp url 3000 | cut -f 3 -d /)\"" >> .env
 echo 'APP_PROTOCOL="https://"' >> .env
-rtx x ruby -- gem install foreman
+gem install foreman
 bin/setup
 bin/webpack
